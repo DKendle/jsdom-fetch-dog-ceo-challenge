9588c4a61ea80b25123d8d15062256e8a89eebd6
DKendle <dina.kendle@gmail.com>
2021-06-05 14:06:16 -0400

    commit tree 2c93ac13398870f8ccdb15dca1bdea9f26c55ea1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 console.log('%c HI', 'color: firebrick')
+let 
+
+
+
+document.addEventListener("DOMContentLoaded", function(e){
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(json){
+        let array = json.message
+        for (const element of array){
+
+        }
+    })
+})
